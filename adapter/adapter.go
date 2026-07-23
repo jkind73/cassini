2d5ef5fc1231d51521705fb25418df30cce77411
Giovanni Gentile <jkind73@gmail.com>
2026-07-23 15:20:07 -0400

    fix(ui): place Sinden Lightgun and multi-controller options in the Input settings screen

--- adapter/adapter.go
+++ adapter/adapter.go
@@ -149,7 +149,7 @@ func (f *Factory) SystemInfo() coreif.SystemInfo {
 				Description: "Enable optical lightgun peripheral targeting",
 				Type:        coreif.CoreOptionBool,
 				Default:     "true",
-				Category:    coreif.CoreOptionCategoryCore,
+				Category:    coreif.CoreOptionCategoryInput,
 			},
 			{
 				Key:         "lightgun_port",
@@ -158,7 +158,7 @@ func (f *Factory) SystemInfo() coreif.SystemInfo {
 				Type:        coreif.CoreOptionSelect,
 				Default:     "Port 1",
 				Values:      []string{"Port 1", "Port 2"},
-				Category:    coreif.CoreOptionCategoryCore,
+				Category:    coreif.CoreOptionCategoryInput,
 			},
 			{
 				Key:         "active_players",
@@ -167,7 +167,7 @@ func (f *Factory) SystemInfo() coreif.SystemInfo {
 				Type:        coreif.CoreOptionSelect,
 				Default:     "2 Players",
 				Values:      []string{"2 Players", "4 Players", "6 Players", "12 Players"},
-				Category:    coreif.CoreOptionCategoryCore,
+				Category:    coreif.CoreOptionCategoryInput,
 			},
 		},
 		BIOSOptions: []coreif.BIOSOption{
